6c88f4d3384ed3d0202a702ed0e058f0b72c5ae0
golu Kumar <golukumarjaisidih@gmail.com>
2024-08-24 16:30:41 +0530

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import Navbar from "./Components/Navbar"
+import Contact from "./Pages/Contact"
+import Education from "./Pages/Education"
 import Home from "./Pages/Home"
+import Projects from "./Pages/Projects"
 
 function App() {
 
   return (
     <div className="bg-black">
+       <Navbar/>
        <Home/>
+       <Education/>
+       <Projects/>
+       <Contact/>
     </div>
   )
 }
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-blue-500 text-white p-4 text-center">
+    <footer className="bg-black text-white p-4 text-center">
       <p>&copy; 2024 My Portfolio. All rights reserved.</p>
     </footer>
   );
