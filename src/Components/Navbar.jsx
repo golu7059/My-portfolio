ef13c9ab0acd0e1c022eab2957fa48a3651faad2
golu Kumar <golukumarjaisidih@gmail.com>
2024-08-24 16:26:32 +0530

    Navbar hover orange added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <header className="bg-black text-white p-4 flex justify-between items-center px-11">
-        <div>
-        <h1 className="text-3xl font-bold">My Portfolio</h1>
+    <header className="bg-black text-white p-4 flex justify-between items-center px-11 fixed w-full">
+        <div className='w-1/3'>
+        <h1 className="text-2xl font-bold">Portfolio</h1>
         </div>
-      <nav className=''>
-        <ul className="space-x-4 flex items-center justify-between gap-6">
-          <li><a href="#about">About</a></li>
-          <li><a href="#projects">Projects</a></li>
-          <li><a href="#contact">Contact</a></li>
+      <nav className='w-1/2 '>
+        <ul className="space-x-4 w-5/6 flex items-center justify-around gap-7">
+          <li className=' hover:text-orange-600 active:text-orange-700 hover:underline'><a href="#Home">Home</a></li>
+          <li className=' hover:text-orange-600 active:text-orange-700 hover:underline'><a href="#About">About</a></li>
+          <li className=' hover:text-orange-600 active:text-orange-700 hover:underline'><a href="#Education">Education</a></li>
+          <li className=' hover:text-orange-600 active:text-orange-700 hover:underline'><a href="#projects">Projects</a></li>
+          <li className=' hover:text-orange-600 active:text-orange-700 hover:underline'><a href="#contact">Contact</a></li>
         </ul>
       </nav>
     </header>
