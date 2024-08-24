290610ead95dd5c5746865423d76ad0a90737a6c
golu Kumar <golukumarjaisidih@gmail.com>
2024-08-24 23:33:58 +0530

    Languages added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import Navbar from "./Components/Navbar"
-import Contact from "./Pages/Contact"
-import Education from "./Pages/Education"
 import Home from "./Pages/Home"
+import About from "./Pages/About"
+import Education from "./Pages/Education"
+import Languages from "./Components/Languages"
 import Projects from "./Pages/Projects"
+import Contact from "./Pages/Contact"
+import Footer from "./Components/Footer"
 
 function App() {
 
@@ -10,9 +13,12 @@ function App() {
     <div className="bg-black">
        <Navbar/>
        <Home/>
+       <About/>
        <Education/>
+       <Languages/>
        <Projects/>
        <Contact/>
+       <Footer/>
     </div>
   )
 }
